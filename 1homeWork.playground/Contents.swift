f8b08366cb0c1b56f1b557a8af172d96fb8e03d6
esergeeva <esergeeva@ivi.ru>
2026-03-18 23:44:20 +0300

    add the first home work

---
+++ 1homeWork.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Создать свои переменные и константы разных типов. UInt, Int, String, Bool, Float, Double
+
+
+let userName = "Elena"
+let userBirthYear = 1998
+let userCountry = "Россия"
+let userLikesSwift = true
+let userID: UInt32 = 4294967295
+
+var userAge = 27
+var lessonsCompleted = 1
+var averageRating: Double = 2.78
+var userProgress: Float = 3.8
+var userOnline = true
+
+
